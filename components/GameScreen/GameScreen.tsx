ac813e4b3ba971b90d3bcef9f7498bcb8daab61e
Sameer Rasa <crazzywizard@gmail.com>
2023-02-12 13:52:36 -0500

    base game screen setup

--- components/GameScreen/GameScreen.tsx
+++ components/GameScreen/GameScreen.tsx
@@ -1,3 +1,50 @@
-const GameScreen = () => <div>GameScreen</div>
+import { useState } from "react"
+
+const GameScreen = () => {
+  const [choices, setChoices] = useState([])
+  return (
+    <div className="flex flex-col h-screen align-center items-center justify-center gap-4">
+      <div className="flex flex-row ">
+        <button
+          type="button"
+          className="p-4 m-2 border-red-600 border-4 rounded-2xl"
+          onClick={() => setChoices([...choices, "drums"])}
+          disabled={choices.length === 2}
+        >
+          Drums
+        </button>
+        <button
+          type="button"
+          className="p-4 m-2 border-green-600 border-4 rounded-2xl"
+          onClick={() => setChoices([...choices, "bass"])}
+          disabled={choices.length === 2}
+        >
+          Bass
+        </button>
+        <button
+          type="button"
+          className="p-4 m-2 border-blue-600 border-4 rounded-2xl"
+          onClick={() => setChoices([...choices, "guitar"])}
+          disabled={choices.length === 2}
+        >
+          Guitar
+        </button>
+        <button
+          type="button"
+          className="p-4 m-2 border-orange-600 border-4 rounded-2xl"
+          onClick={() => setChoices([...choices, "vocal"])}
+          disabled={choices.length === 2}
+        >
+          Vocals
+        </button>
+      </div>
+      {choices.length > 1 && (
+        <div>
+          <button type="button">Remix</button>
+        </div>
+      )}
+    </div>
+  )
+}
 
 export default GameScreen

13cd07cc448626073fa2245fa863cbc3a2857b92
Sweets Sweetman <sweetmantech@gmail.com>
2023-02-16 07:47:47 -0300

    I update landing page with Results component.

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,16 +3,20 @@ import { useState } from "react"
 import dynamic from "next/dynamic"
 import SeoHead from "../components/SeoHead"
 import LaunchPage from "../components/LaunchPage"
+import Results from "../components/Results"
 
 const GameScreen = dynamic(() => import("../components/GameScreen"), { ssr: false })
 const Home: NextPage = () => {
   const [entered, setEntered] = useState(false)
+  const [minted, setMinted] = useState(true)
 
   return (
     <div className="bg-[#010e17] text-white">
       <SeoHead title="Music Game" description="Music Game coming soon" image="" />
 
-      {entered ? <GameScreen /> : <LaunchPage onClick={setEntered} />}
+      {!entered && <LaunchPage onClick={setEntered} />}
+      {entered && !minted && <GameScreen />}
+      {minted && <Results />}
     </div>
   )
 }

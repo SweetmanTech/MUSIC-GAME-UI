21c5d9e5eca5fd77b29a05473c812f07990c72e8
sameer rasa <crazzywizard@gmail.com>
2023-02-18 22:23:30 -0500

    created remix api

--- pages/api/remix/index.ts
+++ pages/api/remix/index.ts
@@ -1,24 +1,28 @@
+import { NFTStorage } from "nft.storage"
 /* eslint-disable class-methods-use-this */
 import { createHandler, Post, Body } from "next-api-decorators"
+import path from "path"
+import { readFileSync } from "fs"
+import { Blob } from "buffer"
+
+const client = new NFTStorage({
+  token: `${process?.env?.NEXT_PUBLIC_NFT_STORAGE_TOKEN}`,
+})
 
 const ffmpegStatic = require("ffmpeg-static")
 const ffmpeg = require("fluent-ffmpeg")
 
 ffmpeg.setFfmpegPath(ffmpegStatic)
 
-const mixAudio = (track1: string, track2: string) =>
+const mixAudio = (track1: string, track2: string, output: string) =>
   new Promise((resolve, reject) => {
     ffmpeg()
       .input(track1)
       .input(track2)
       .outputOptions("-filter_complex", "amix=inputs=2:duration=longest")
-      .saveToFile("output.wav")
+      .saveToFile(output)
       // Log the percentage of work completed
-      .on("progress", (progress) => {
-        if (progress.percent) {
-          console.log(`Processing: ${Math.floor(progress.percent)}% done`)
-        }
-      })
+      .on("progress", () => null)
       // The callback that is run when FFmpeg is finished
       .on("end", () => resolve("done"))
 
@@ -29,10 +33,13 @@ class Remix {
   @Post()
   async remix(@Body() body: { track1: string; track2: string }) {
     const { track1, track2 } = body
-
-    const response = await mixAudio(track1, track2)
-
-    return response
+    const currentPath = path.resolve(process.cwd())
+    const outputFile = path.join(currentPath, "output.wav")
+    await mixAudio(track1, track2, outputFile)
+    const audioFile = readFileSync(outputFile)
+    const fileToBlob = new Blob([audioFile], { type: "audio/wav" })
+    const CID = await client.storeBlob(fileToBlob as any)
+    return { CID }
   }
 }
 

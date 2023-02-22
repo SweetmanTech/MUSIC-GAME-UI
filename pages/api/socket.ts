39e94fa96ca1bdf658fe3849f7094ad0b059ebd9
sameer rasa <crazzywizard@gmail.com>
2023-02-21 21:18:39 -0500

    api for socket connection setup

--- pages/api/socket.ts
+++ pages/api/socket.ts
@@ -5,17 +5,19 @@ import { Server } from "socket.io"
 const SocketHandler = (req, res) => {
   if (res.socket.server.io) {
     console.log("Socket is already running")
-  } else {
-    console.log("Socket is initializing")
-    const io = new Server(res.socket.server)
-    res.socket.server.io = io
-
-    io.on("connection", (socket) => {
-      socket.on("input-change", (msg) => {
-        socket.broadcast.emit("update-input", msg)
-      })
-    })
+    res.end()
+    return
   }
+  const io = new Server(res.socket.server)
+  res.socket.server.io = io
+
+  io.on("connection", (socket) => {
+    socket.on("newChatMessage", (data) => {
+      console.log("newChatMessage", data)
+      io.emit("newChatMessage", data)
+    })
+  })
+  console.log("Setting up socket")
   res.end()
 }
 

0baaa400ffea9ba1a4b6077deb09852c28c43da5
sameer rasa <crazzywizard@gmail.com>
2023-02-21 21:18:27 -0500

    front end chat box feature

---
+++ hooks/useChat.tsx
@@ -0,0 +1,44 @@
+import { useCallback, useEffect, useRef, useState } from "react"
+
+import { io } from "socket.io-client"
+
+const NEW_CHAT_MESSAGE_EVENT = "newChatMessage" // Name of the event
+
+const useChat = () => {
+  const [messages, setMessages] = useState([])
+  const socketRef = useRef(null) // Sent and received messages
+  const socketInitializer = useCallback(async (): Promise<void> => {
+    await fetch("/api/socket")
+    socketRef.current = io()
+    socketRef.current.on(NEW_CHAT_MESSAGE_EVENT, (message) => {
+      const incomingMessage = {
+        ...message,
+        ownedByCurrentUser: message.senderId === socketRef.current.id,
+      }
+      setMessages([...messages, incomingMessage])
+    })
+  }, [messages])
+
+  const disconnectSocket = useCallback(() => {
+    if (socketRef.current) {
+      socketRef.current.disconnect()
+    }
+  }, [])
+  useEffect(() => {
+    socketInitializer()
+    return () => {
+      disconnectSocket()
+    }
+  }, [socketInitializer, disconnectSocket])
+  // Sends a message to the server that
+  // forwards it to all users in the same room
+  const sendMessage = (messageBody) => {
+    socketRef.current.emit(NEW_CHAT_MESSAGE_EVENT, {
+      body: messageBody,
+      senderId: socketRef.current.id,
+    })
+  }
+  return { messages, sendMessage }
+}
+
+export default useChat

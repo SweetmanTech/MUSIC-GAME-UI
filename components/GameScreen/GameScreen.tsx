d527e70a83fe5fc0b803a1b09ff93b72187dc3b0
sameer rasa <crazzywizard@gmail.com>
2023-02-24 20:16:38 -0500

    added ability to increase number of tracks to remix added ability to listen to all tracks at once

--- components/GameScreen/GameScreen.tsx
+++ components/GameScreen/GameScreen.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react"
+import { useCallback, useEffect, useState, useMemo } from "react"
 import Image from "next/image"
 import _ from "lodash"
 import MintButton from "../MintButton"
@@ -16,6 +16,7 @@ interface IChecked {
   guitar: boolean
 }
 const GameScreen = ({ onSuccess }: any) => {
+  const context = useMemo(() => new AudioContext(), [])
   const [choices, setChoices] = useState<Array<string>>([])
   const [checked, setChecked] = useState<IChecked>({
     drums: false,
@@ -24,12 +25,7 @@ const GameScreen = ({ onSuccess }: any) => {
     guitar: false,
   })
   const [playAudio, setPlayAudio] = useState<boolean>(false)
-  const [instrumentChoice, setInstrumentChoice] = useState<{
-    drums: string | null
-    vocal: string | null
-    bass: string | null
-    guitar: string | null
-  }>({ drums: null, vocal: null, bass: null, guitar: null })
+  const [chosenAudioTracks, setChosenAudioTrack] = useState<Array<string>>([])
   const options: IOption[] = [
     { id: "bass", name: "Bass", imgUrl: "/bass.png" },
     { id: "drums", name: "Drums", imgUrl: "/drums.png" },
@@ -37,25 +33,43 @@ const GameScreen = ({ onSuccess }: any) => {
     { id: "vocal", name: "Vocals", imgUrl: "/vocal.png" },
   ]
   const onClickHandler = (value: string) => {
+    const musicChoice = _.sample(MUSIC_URLS[value])
     if (choices.includes(value)) {
       setChoices([...choices.filter((e) => e !== value)])
+      setChosenAudioTrack([...chosenAudioTracks.filter((e) => e !== musicChoice)])
     } else {
       setChoices([...choices, value])
+      setChosenAudioTrack([...chosenAudioTracks, musicChoice])
     }
     setChecked({ ...checked, [value]: !checked[value] })
   }
-  const [musicUrl, setMusicUrl] = useState<string>("")
-  const onMouseOverHandler = (value: string) => {
-    if (checked[value]) return
-    const musicChoice = _.sample(MUSIC_URLS[value])
-    setInstrumentChoice({
-      ...instrumentChoice,
-      [value]: musicChoice,
-    })
-    setMusicUrl(musicChoice)
-    setPlayAudio(true)
-  }
-
+  const play = useCallback(
+    (audioBuffer: AudioBuffer) => {
+      const source = context.createBufferSource()
+      source.buffer = audioBuffer
+      source.connect(context.destination)
+      source.start()
+    },
+    [context],
+  )
+  const fetchAudio = useCallback(
+    async (url: string) => {
+      const response = await fetch(url)
+      const arrayBuffer = await response.arrayBuffer()
+      const audioBuffer = await context.decodeAudioData(arrayBuffer)
+      return audioBuffer
+    },
+    [context],
+  )
+  useEffect(() => {
+    if ((chosenAudioTracks.length, playAudio)) {
+      chosenAudioTracks.forEach((track) => {
+        fetchAudio(track).then((audioBuffer) => {
+          play(audioBuffer)
+        })
+      })
+    }
+  }, [chosenAudioTracks, fetchAudio, play, playAudio])
   return (
     <div className="flex flex-col items-center justify-center h-screen gap-4 p-4 align-center">
       <div className="p-4 m-4 font-mono text-2xl font-extrabold text-gray-900 bg-white rounded-md">
@@ -71,28 +85,62 @@ const GameScreen = ({ onSuccess }: any) => {
                 checked[option.id] ? `border-green-500` : "border-white-500"
               }  ${checked[option.id] ? "border-4" : "border-4"} rounded-full disabled:opacity-25`}
               onClick={() => onClickHandler(option.id)}
-              onMouseEnter={() => {
-                onMouseOverHandler(option.id)
-              }}
-              onMouseLeave={() => {
-                setPlayAudio(false)
-              }}
-              disabled={choices.length === 2 && !choices.includes(option.id)}
             >
               <Image src={option.imgUrl} alt={option.name} width={100} height={100} />
             </button>
-            {playAudio && musicUrl && !checked[option.id] && (
-              <audio autoPlay>
-                <source src={musicUrl} type="audio/mpeg" />
-                <track kind="captions" />
-              </audio>
-            )}
           </div>
         ))}
       </div>
-
+      {chosenAudioTracks.length > 0 &&
+        (!playAudio ? (
+          <button
+            type="button"
+            onClick={() => {
+              setPlayAudio(true)
+              context.resume()
+            }}
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth={1.5}
+              stroke="currentColor"
+              className="w-6 h-6"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M5.25 5.653c0-.856.917-1.398 1.667-.986l11.54 6.348a1.125 1.125 0 010 1.971l-11.54 6.347a1.125 1.125 0 01-1.667-.985V5.653z"
+              />
+            </svg>
+          </button>
+        ) : (
+          <button
+            type="button"
+            onClick={() => {
+              setPlayAudio(false)
+              context.suspend()
+            }}
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke-width="1.5"
+              stroke="currentColor"
+              className="w-6 h-6"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                d="M15.75 5.25v13.5m-7.5-13.5v13.5"
+              />
+            </svg>
+          </button>
+        ))}
       {choices.length > 1 && (
-        <MintButton choices={choices} onSuccess={onSuccess} instrumentUrl={instrumentChoice} />
+        <MintButton onSuccess={onSuccess} audioTracksToMix={chosenAudioTracks} />
       )}
     </div>
   )

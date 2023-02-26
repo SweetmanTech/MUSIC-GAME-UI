678e5928acc99c4b1744035f23cf99d481d2aecd
sameer rasa <crazzywizard@gmail.com>
2023-02-26 10:23:24 -0500

    added flex wrap for mobile

--- components/GameScreen/GameScreen.tsx
+++ components/GameScreen/GameScreen.tsx
@@ -150,17 +150,17 @@ const GameScreen = ({ onSuccess }: any) => {
     }
   }, [chosenAudioTracks, fetchAudio, playTracks, playAudio, context, stopAudio])
   return (
-    <div className="flex flex-col items-center justify-center h-screen gap-4 p-4 align-center">
+    <div className="flex flex-col items-center justify-center min-h-screen gap-4 p-4 align-center">
       <div className="p-4 m-4 font-mono text-2xl font-extrabold text-gray-900 bg-white rounded-md">
         Pick any music, you can click play to hear possible choice.
       </div>
-      <div className="flex flex-row">
+      <ul className="flex flex-wrap overflow-x-auto">
         {options.map((option) => (
-          <div key={option.id}>
+          <li key={option.id} className="flex-none">
             <button
               key={option.id}
               type="button"
-              className={`p-4 m-2 ${
+              className={`p-4 m-2 flex-none ${
                 checked[option.id] ? `border-green-500` : "border-white-500"
               }  ${checked[option.id] ? "border-4" : "border-4"} rounded-full disabled:opacity-25 ${
                 loadingAssets && "animate-pulse bg-gray-400 cursor-not-allowed"
@@ -172,9 +172,9 @@ const GameScreen = ({ onSuccess }: any) => {
                 <Image src={option.imgUrl} alt={option.name} width={100} height={100} />
               )}
             </button>
-          </div>
+          </li>
         ))}
-      </div>
+      </ul>
       <div className="flex flex-row-reverse gap-4">
         {chosenAudioTracks.length > 0 &&
           (!playAudio ? (

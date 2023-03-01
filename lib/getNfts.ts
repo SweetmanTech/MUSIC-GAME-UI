0dcd3288b4b0d33cefa16a7de24e7e565d65da0e
Sweets Sweetman <sweetmantech@gmail.com>
2023-03-01 13:48:22 -0300

    I fix linting errors.

--- lib/getNfts.ts
+++ lib/getNfts.ts
@@ -1,12 +1,11 @@
-import axios from "axios";
+import axios from "axios"
 
-const getNfts = async(owner: string, contractAddress: string) => {
+const getNfts = async (owner: string, contractAddress: string) => {
   const alchemyKey = "ezyXM9BT43gERc4t37pvrR29sDYX81Ph"
-  const {data} = await axios.get(
+  const { data } = await axios.get(
     `https://eth-goerli.g.alchemy.com/nft/v2/${alchemyKey}/getNFTs?owner=${owner}&contractAddresses%5B%5D=${contractAddress}`
   )
-  console.log("data", data)
   return data
 }
 
-export default getNfts;
+export default getNfts
